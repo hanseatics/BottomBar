3e265d2367e861b11e8ec8a192f756a3a8a06839
Iiro <iiro.krankka@gmail.com>
2016-03-17 14:26:36 +0200

    Support for tablets.

--- bottom-bar/src/main/java/com/roughike/bottombar/BottomBar.java
+++ bottom-bar/src/main/java/com/roughike/bottombar/BottomBar.java
@@ -3,6 +3,7 @@ package com.roughike.bottombar;
 import android.annotation.TargetApi;
 import android.app.Activity;
 import android.content.Context;
+import android.graphics.PorterDuff;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.IdRes;
@@ -45,7 +46,9 @@ public class BottomBar extends FrameLayout implements View.OnClickListener, View
     private static final String TAG_BOTTOM_BAR_VIEW_ACTIVE = "BOTTOM_BAR_VIEW_ACTIVE";
 
     private Context mContext;
+    private boolean mIsTabletMode;
 
+    private RelativeLayout mRootView;
     private FrameLayout mUserContentContainer;
     private LinearLayout mItemContainer;
 
@@ -53,6 +56,7 @@ public class BottomBar extends FrameLayout implements View.OnClickListener, View
     private int mInActiveColor;
     private int mWhiteColor;
 
+    private int mScreenWidth;
     private int mTwoDp;
     private int mTenDp;
     private int mMaxFixedItemWidth;
@@ -96,6 +100,7 @@ public class BottomBar extends FrameLayout implements View.OnClickListener, View
         mInActiveColor = ContextCompat.getColor(mContext, R.color.bb_inActiveBottomBarItemColor);
         mWhiteColor = ContextCompat.getColor(mContext, R.color.white);
 
+        mScreenWidth = MiscUtils.getScreenWidth(mContext);
         mTwoDp = MiscUtils.dpToPixel(mContext, 2);
         mTenDp = MiscUtils.dpToPixel(mContext, 10);
         mMaxFixedItemWidth = MiscUtils.dpToPixel(mContext, 168);
@@ -108,13 +113,14 @@ public class BottomBar extends FrameLayout implements View.OnClickListener, View
                 LayoutParams.WRAP_CONTENT);
         setLayoutParams(params);
 
-        RelativeLayout itemContainerRoot = (RelativeLayout) View.inflate(mContext,
+        mRootView = (RelativeLayout) View.inflate(mContext,
                 R.layout.bb_bottom_bar_item_container, null);
 
-        mUserContentContainer = (FrameLayout) itemContainerRoot.findViewById(R.id.bb_user_content_container);
-        mItemContainer = (LinearLayout) itemContainerRoot.findViewById(R.id.bb_bottom_bar_item_container);
+        mIsTabletMode = mRootView.findViewById(R.id.bb_tablet_right_border) != null;
+        mUserContentContainer = (FrameLayout) mRootView.findViewById(R.id.bb_user_content_container);
+        mItemContainer = (LinearLayout) mRootView.findViewById(R.id.bb_bottom_bar_item_container);
 
-        addView(itemContainerRoot, params);
+        addView(mRootView, params);
     }
 
     protected FrameLayout getUserContainer() {
@@ -282,7 +288,7 @@ public class BottomBar extends FrameLayout implements View.OnClickListener, View
 
     @Override
     public boolean onLongClick(View v) {
-        if (mIsShiftingMode && v.getTag().equals(TAG_BOTTOM_BAR_VIEW_INACTIVE)) {
+        if ((mIsShiftingMode || mIsTabletMode) && v.getTag().equals(TAG_BOTTOM_BAR_VIEW_INACTIVE)) {
             Toast.makeText(mContext, mItems[findItemPosition(v)].getTitle(mContext), Toast.LENGTH_SHORT).show();
         }
 
@@ -294,23 +300,32 @@ public class BottomBar extends FrameLayout implements View.OnClickListener, View
         int biggestWidth = 0;
         mIsShiftingMode = MAX_FIXED_TAB_COUNT < bottomBarItems.length;
 
-        if (mIsShiftingMode) {
+        if (!mIsTabletMode && mIsShiftingMode) {
             mItemContainer.setBackgroundColor(mPrimaryColor);
         }
 
         View[] viewsToAdd = new View[bottomBarItems.length];
 
         for (BottomBarItemBase bottomBarItemBase : bottomBarItems) {
-            ViewGroup bottomBarView = (ViewGroup) View.inflate(mContext, mIsShiftingMode ?
-                    R.layout.bb_bottom_bar_item_shifting : R.layout.bb_bottom_bar_item_fixed, null);
-
-            ImageView icon = (ImageView) bottomBarView.findViewById(R.id.bb_bottom_bar_icon);
-            TextView title = (TextView) bottomBarView.findViewById(R.id.bb_bottom_bar_title);
-
-            icon.setImageDrawable(bottomBarItemBase.getIcon(mContext));
-            title.setText(bottomBarItemBase.getTitle(mContext));
+            int layoutResource;
 
             if (mIsShiftingMode) {
+                layoutResource = R.layout.bb_bottom_bar_item_shifting;
+            } else {
+                layoutResource = R.layout.bb_bottom_bar_item_fixed;
+            }
+
+            View bottomBarView = View.inflate(mContext, layoutResource, null);
+            ImageView icon = (ImageView) bottomBarView.findViewById(R.id.bb_bottom_bar_icon);
+
+            icon.setImageDrawable(bottomBarItemBase.getIcon(mContext));
+
+            if (!mIsTabletMode) {
+                TextView title = (TextView) bottomBarView.findViewById(R.id.bb_bottom_bar_title);
+                title.setText(bottomBarItemBase.getTitle(mContext));
+            }
+
+            if (!mIsTabletMode && mIsShiftingMode) {
                 icon.setColorFilter(mWhiteColor);
             }
 
@@ -324,28 +339,34 @@ public class BottomBar extends FrameLayout implements View.OnClickListener, View
                 unselectTab(bottomBarView, false);
             }
 
-            if (bottomBarView.getWidth() > biggestWidth) {
-                biggestWidth = bottomBarView.getWidth();
+            if (!mIsTabletMode) {
+                if (bottomBarView.getWidth() > biggestWidth) {
+                    biggestWidth = bottomBarView.getWidth();
+                }
+
+                viewsToAdd[index] = bottomBarView;
+            } else {
+                mItemContainer.addView(bottomBarView);
             }
 
             bottomBarView.setOnClickListener(this);
             bottomBarView.setOnLongClickListener(this);
-            viewsToAdd[index] = bottomBarView;
             index++;
         }
 
-        int screenWidth = MiscUtils.getScreenWidth(mContext);
-        int proposedItemWidth = Math.min(
-                MiscUtils.dpToPixel(mContext, screenWidth / bottomBarItems.length),
-                mMaxFixedItemWidth
-        );
+        if (!mIsTabletMode) {
+            int proposedItemWidth = Math.min(
+                    MiscUtils.dpToPixel(mContext, mScreenWidth / bottomBarItems.length),
+                    mMaxFixedItemWidth
+            );
 
-        LinearLayout.LayoutParams params = new LinearLayout
-                .LayoutParams(proposedItemWidth, LinearLayout.LayoutParams.WRAP_CONTENT);
+            LinearLayout.LayoutParams params = new LinearLayout
+                    .LayoutParams(proposedItemWidth, LinearLayout.LayoutParams.WRAP_CONTENT);
 
-        for (View bottomBarView : viewsToAdd) {
-            bottomBarView.setLayoutParams(params);
-            mItemContainer.addView(bottomBarView);
+            for (View bottomBarView : viewsToAdd) {
+                bottomBarView.setLayoutParams(params);
+                mItemContainer.addView(bottomBarView);
+            }
         }
 
         updateCurrentFragment();
@@ -364,14 +385,21 @@ public class BottomBar extends FrameLayout implements View.OnClickListener, View
         }
     }
 
-    private void selectTab(ViewGroup bottomBarView, boolean animate) {
+    private void selectTab(View bottomBarView, boolean animate) {
         bottomBarView.setTag(TAG_BOTTOM_BAR_VIEW_ACTIVE);
         ImageView icon = (ImageView) bottomBarView.findViewById(R.id.bb_bottom_bar_icon);
         TextView title = (TextView) bottomBarView.findViewById(R.id.bb_bottom_bar_title);
 
-        if (!mIsShiftingMode) {
+        if (!mIsShiftingMode || mIsTabletMode) {
             icon.setColorFilter(mPrimaryColor);
-            title.setTextColor(mPrimaryColor);
+
+            if (title != null) {
+                title.setTextColor(mPrimaryColor);
+            }
+        }
+
+        if (title == null) {
+            return;
         }
 
         int translationY = mIsShiftingMode ? mTenDp : mTwoDp;
@@ -404,14 +432,22 @@ public class BottomBar extends FrameLayout implements View.OnClickListener, View
         }
     }
 
-    private void unselectTab(ViewGroup bottomBarView, boolean animate) {
+    private void unselectTab(View bottomBarView, boolean animate) {
         bottomBarView.setTag(TAG_BOTTOM_BAR_VIEW_INACTIVE);
+
         ImageView icon = (ImageView) bottomBarView.findViewById(R.id.bb_bottom_bar_icon);
         TextView title = (TextView) bottomBarView.findViewById(R.id.bb_bottom_bar_title);
 
-        if (!mIsShiftingMode) {
+        if (!mIsShiftingMode || mIsTabletMode) {
             icon.setColorFilter(mInActiveColor);
-            title.setTextColor(mInActiveColor);
+
+            if (title != null) {
+                title.setTextColor(mInActiveColor);
+            }
+        }
+
+        if (title == null) {
+            return;
         }
 
         float scale = mIsShiftingMode ? 0 : 0.86f;
@@ -427,7 +463,7 @@ public class BottomBar extends FrameLayout implements View.OnClickListener, View
                     .translationY(0)
                     .start();
 
-            if (mIsShiftingMode) {
+            if (mIsShiftingMode && !mIsTabletMode) {
                 icon.animate()
                         .setDuration(ANIMATION_DURATION)
                         .alpha(0.6f)
@@ -438,7 +474,7 @@ public class BottomBar extends FrameLayout implements View.OnClickListener, View
             title.setScaleY(scale);
             bottomBarView.setTranslationY(0);
 
-            if (mIsShiftingMode) {
+            if (mIsShiftingMode && !mIsTabletMode) {
                 icon.setAlpha(0.6f);
             }
         }
