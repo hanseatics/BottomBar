e5cfa68a872935ab41ed5a79acffdef2f13ae58e
Iiro <iiro.krankka@gmail.com>
2016-03-19 13:22:39 +0200

    Now should detect Navigation Bar presence correctly.

--- bottom-bar/src/main/java/com/roughike/bottombar/BottomBar.java
+++ bottom-bar/src/main/java/com/roughike/bottombar/BottomBar.java
@@ -698,17 +698,21 @@ public class BottomBar extends FrameLayout implements View.OnClickListener, View
                 .getIdentifier("config_showNavigationBar", "bool", "android");
 
         if (!bottomBar.drawBehindNavBar()
-                || !(softMenuIdentifier > 0 && res.getBoolean(softMenuIdentifier))) {
-            return;
-        }
-
-        if (ViewConfiguration.get(activity).hasPermanentMenuKey() && KeyCharacterMap.deviceHasKey(KeyEvent.KEYCODE_BACK)) {
+                || (!(softMenuIdentifier > 0 && res.getBoolean(softMenuIdentifier))
+                && ViewConfiguration.get(activity).hasPermanentMenuKey())) {
             return;
         }
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT
                 && res.getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT) {
             activity.getWindow().getAttributes().flags |= WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION;
+            activity.getWindow().getDecorView().setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                activity.getWindow().getAttributes().flags |=
+                        WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS;
+            }
 
             int navBarIdentifier = res.getIdentifier("navigation_bar_height",
                     "dimen", "android");
