a61b6a9f4349276451380817d1972b6693be345e
Arvind Menon <arvind.menon@sportngin.com>
2016-03-23 15:11:44 -0500

    Adding a listener to listen for tabs that are reselected

--- bottom-bar/src/main/java/com/roughike/bottombar/BottomBar.java
+++ bottom-bar/src/main/java/com/roughike/bottombar/BottomBar.java
@@ -878,23 +878,35 @@ public class BottomBar extends FrameLayout implements View.OnClickListener, View
         if (v.getTag().equals(TAG_BOTTOM_BAR_VIEW_INACTIVE)) {
             unselectTab(findViewWithTag(TAG_BOTTOM_BAR_VIEW_ACTIVE), true);
             selectTab(v, true);
-            updateSelectedTab(findItemPosition(v));
         }
+        updateSelectedTab(findItemPosition(v));
     }
 
     private void updateSelectedTab(int newPosition) {
+        final boolean canNotifyMenuListener = mMenuListener != null && mItems instanceof BottomBarTab[];
+        final boolean canNotifyListener = mListener != null;
+
         if (newPosition != mCurrentTabPosition) {
             handleBadgeVisibility(mCurrentTabPosition, newPosition);
             mCurrentTabPosition = newPosition;
+
+            if (canNotifyListener) {
+                mListener.onItemSelected(mCurrentTabPosition);
+            }
+
+            if (canNotifyMenuListener) {
+                mMenuListener.onMenuItemSelected(((BottomBarTab) mItems[mCurrentTabPosition]).id);
+            }
+
             updateCurrentFragment();
-        }
+        } else {
+            if (canNotifyListener && mListener instanceof OnTabItemClickListener) {
+                ((OnTabItemClickListener) mListener).onItemReSelected(mCurrentTabPosition);
+            }
 
-        if (mListener != null) {
-            mListener.onItemSelected(mCurrentTabPosition);
-        }
-
-        if (mMenuListener != null && mItems instanceof BottomBarTab[]) {
-            mMenuListener.onMenuItemSelected(((BottomBarTab) mItems[mCurrentTabPosition]).id);
+            if (canNotifyMenuListener && mMenuListener instanceof OnMenuTabItemClickListener) {
+                ((OnMenuTabItemClickListener) mMenuListener).onMenuItemReSelected(((BottomBarTab) mItems[mCurrentTabPosition]).id);
+            }
         }
     }
 
---
+++ bottom-bar/src/main/java/com/roughike/bottombar/OnTabItemClickListener.java
@@ -0,0 +1,28 @@
+package com.roughike.bottombar;
+
+/*
+ * BottomBar library for Android
+ * Copyright (c) 2016 Iiro Krankka (http://github.com/roughike).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+public interface OnTabItemClickListener extends OnTabSelectedListener {
+    /**
+     * The method being called when currently visible {@link BottomBarTab} is
+     * reselected.
+     *
+     * @param position the currently selected {@link BottomBarTab}
+     */
+    void onItemReSelected(int position);
+}
