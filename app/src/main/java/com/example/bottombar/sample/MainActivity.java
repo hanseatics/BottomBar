9a7246dd73674c66dee124316e19c8220662b8be
Iiro <iiro.krankka@gmail.com>
2016-03-25 12:22:10 +0200

    Updated sample.

--- app/src/main/java/com/example/bottombar/sample/MainActivity.java
+++ app/src/main/java/com/example/bottombar/sample/MainActivity.java
@@ -1,28 +1,38 @@
 package com.example.bottombar.sample;
 
 import android.os.Bundle;
+import android.support.annotation.IdRes;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.roughike.bottombar.BottomBar;
-import com.roughike.bottombar.BottomBarFragment;
+import com.roughike.bottombar.OnMenuTabClickListener;
 
 public class MainActivity extends AppCompatActivity {
     private BottomBar mBottomBar;
+    private TextView mMessageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mMessageView = (TextView) findViewById(R.id.messageView);
+
         mBottomBar = BottomBar.attach(this, savedInstanceState);
-        mBottomBar.setFragmentItems(getSupportFragmentManager(), R.id.fragmentContainer,
-                new BottomBarFragment(SampleFragment.newInstance("Content for recents."), R.drawable.ic_recents, "Recents"),
-                new BottomBarFragment(SampleFragment.newInstance("Content for favorites."), R.drawable.ic_favorites, "Favorites"),
-                new BottomBarFragment(SampleFragment.newInstance("Content for nearby stuff."), R.drawable.ic_nearby, "Nearby"),
-                new BottomBarFragment(SampleFragment.newInstance("Content for friends."), R.drawable.ic_friends, "Friends"),
-                new BottomBarFragment(SampleFragment.newInstance("Content for food."), R.drawable.ic_restaurants, "Food")
-        );
+        mBottomBar.setItemsFromMenu(R.menu.bottombar_menu, new OnMenuTabClickListener() {
+            @Override
+            public void onMenuTabSelected(@IdRes int menuItemId) {
+                mMessageView.setText(getMessage(menuItemId, false));
+            }
+
+            @Override
+            public void onMenuTabReSelected(@IdRes int menuItemId) {
+                Toast.makeText(getApplicationContext(), getMessage(menuItemId, true), Toast.LENGTH_SHORT).show();
+            }
+        });
 
         // Setting colors for different tabs when there's more than three of them.
         // You can set colors for tabs in three different ways as shown below.
@@ -31,6 +41,34 @@ public class MainActivity extends AppCompatActivity {
         mBottomBar.mapColorForTab(2, "#7B1FA2");
         mBottomBar.mapColorForTab(3, "#FF5252");
         mBottomBar.mapColorForTab(4, "#FF9800");
+    }
+
+    private String getMessage(int menuItemId, boolean isReselection) {
+        String message = "Content for ";
+
+        switch (menuItemId) {
+            case R.id.bb_menu_recents:
+                message += "recents";
+                break;
+            case R.id.bb_menu_favorites:
+                message += "favorites";
+                break;
+            case R.id.bb_menu_nearby:
+                message += "nearby";
+                break;
+            case R.id.bb_menu_friends:
+                message += "friends";
+                break;
+            case R.id.bb_menu_food:
+                message += "food";
+                break;
+        }
+
+        if (isReselection) {
+            message += " WAS RESELECTED! YAY!";
+        }
+
+        return message;
     }
 
     @Override
