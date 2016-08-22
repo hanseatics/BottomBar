e4da4cc57a1c4146263c91092ce9c627f6115b1b
Iiro Krankka <iiro.krankka@gmail.com>
2016-08-22 18:19:10 +0300

    Formatting

---
+++ bottom-bar/src/androidTest/java/com/roughike/bottombar/BottomBarTabTest.java
@@ -0,0 +1,17 @@
+package com.roughike.bottombar;
+
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * Created by iiro on 22.8.2016.
+ */
+@RunWith(AndroidJUnit4.class)
+public class BottomBarTabTest {
+    @Test
+    public void test() {
+
+    }
+}
